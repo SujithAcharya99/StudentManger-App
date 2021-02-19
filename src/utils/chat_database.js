8e6b61b537d702aca476333cec5a5e209e6765a8
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2021-02-19 12:27:54 +0530

    Update chat_database.js

--- src/utils/chat_database.js
+++ src/utils/chat_database.js
@@ -1,86 +1,25 @@
-// const mongoose = require('mongoose');
-// const validator = require('validator')
-
-// // const Test = mongoose.model('Test', {
-// const testSchema = new mongoose.Schema({
-//     studentName: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     subject: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     teacherName: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },score: {
-//         type: Number,
-//         required: true,
-//         default: 00
-//     },
-//     questions: [{
-//         mcq_question: {
-//             type: String,
-//             // required: true
-//         },
-//         options: [{
-//             type: String,
-//             required: true
-//         }]
-//     }],
-//     answer: [{
-//         type: String,
-//         required: true
-//     }]
-// });
-
-
-// //const Test = mongoose.model('Test', testSchema);
-
-// //module.exports = Test;
-
-
-
-
 const users = [];
-
-
-//adduser, removeUser, Getuser, getusersinroom
-
 const addUser = ({ id, username, room }) => {
-  //clear the data 
+ 
   username = username.trim().toLowerCase(),
     room = room.trim().toLowerCase()
 
-  //validate the data
   if (!username || !room) {
     return {
       error: 'username and room are required'
     }
   }
-
-  //check for existing user 
   const existingUser = users.find((user) => {
     return user.room === room && user.username === username
   })
-
-  //validate username
   if (existingUser) {
     return {
       error: 'Username is in use..!'
     }
   }
-
-  // store user 
   const user = { id, username, room }
   users.push(user)
-  // console.log(users)
   return { user }
-
 }
 
 const removeUser = (id) => {
@@ -98,43 +37,6 @@ const getUser = (id) => {
 const getusersInRoom = (room) => {
   return users.filter((user) => user.room === room)
 }
-
-// addUser({
-//     id: 22,
-//     username: 'sujith',
-//     room: 'odepoi'
-// })
-
-// addUser({
-//     id: 23,
-//     username: 'sanjay',
-//     room: 'odepoi'
-// })
-// addUser({
-//     id: 12,
-//     username: 'sharath',
-//     room: 'ijes'
-// })
-// // console.log(users)
-
-// const res = addUser({
-//     id:22,
-//     username: 'sujith',
-//     room:'odepoi'
-// })
-
-// const removedUsers = removeUser(22);
-
-// console.log(removedUsers);
-
-// console.log(users)
-
-// const user = getUser(12)
-// console.log(user)
-
-// const userlist = getusersInRoom('iqjes')
-
-// console.log(userlist)
 
 const generateMessage = (username, text) => {
   return {
